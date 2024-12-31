a8007a112b850c1d38e479ea5c03f050a616eacd
Adzka <adzka2008@gmail.com>
2024-12-31 10:08:34 +0700

    Remove deprecated future router options from App.js

--- src/App.js
+++ src/App.js
@@ -34,16 +34,6 @@ const router = createBrowserRouter([
   {
     path: "/add",
     element: <AddPage />,
-  },
-  {
-    future: {
-      v7_startTransition: true,
-      v7_relativeSplatPath: true,
-      v7_fetcherPersist: true,
-      v7_normalizeFormMethod: true,
-      v7_partialHydration: true,
-      v7_skipActionErrorRevalidation: true,
-    },
   }
 ]);
 

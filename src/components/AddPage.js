054887e06c86c1ef8d09228a9fc2ee683157779c
Adzka <adzka2008@gmail.com>
2025-01-01 09:38:18 +0700

    Refactor AddPage and App.css for improved styling and layout; adjust button widths and input fields for better responsiveness

--- src/components/AddPage.js
+++ src/components/AddPage.js
@@ -23,18 +23,18 @@ export default function AddPage() {
 
     return (
         <div className="add-form">
-            <div><input
+            <input
                 className="custom-input"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 placeholder="Name"
-            /></div>
-            <div><input
+            />
+            <input
                 className="custom-input"
                 value={phone}
                 onChange={(e) => setPhone(e.target.value)}
                 placeholder="Phone"
-            /></div>
+            />
             <div>
                 <button className="add-page-button" onClick={handleSubmit}>save</button>
                 <button className="add-page-button" onClick={() => navigate('/')}>cancel</button>
